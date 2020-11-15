1842da442c3d09bbb73ccb1bf94f373e57952c07
Andrew Seward <awswrd@gmail.com>
2020-11-15 09:56:34 -0800

    Move dhall installation to runtime
    Couldn't find a decent way to allow specifying a particular version @
    build, so we'll have to do it at runtime using inputs.
    
    See: https://github.community/t/feature-request-build-args-support-in-docker-container-actions/16846

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,21 @@
 
 set -euo pipefail
 
+_install_dhall() {
+  local dhall_haskell_ver; dhall_haskell_ver="$1"
+  local bin_zip_name; bin_zip_name="dhall-${dhall_haskell_ver}-x86_64-linux.tar.bz2"
+
+  wget "https://github.com/dhall-lang/dhall-haskell/releases/download/${dhall_haskell_ver}/${bin_zip_name}" \
+    && tar -xjvf "./${bin_zip_name}" \
+    && rm -rvf "./${bin_zip_name}"
+  mv ./bin/dhall /bin
+
+  which dhall
+  dhall --version && dhall --help
+}
+
+_install_dhall "$1"
+
 temp_file="$(mktemp)"
 
 collect_errors() {
